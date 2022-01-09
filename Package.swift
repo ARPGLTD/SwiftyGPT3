7ca5c6344326d0cd06f3d2f9d9984f1224470203
pcbeard <pcbeard@mac.com>
2022-01-08 23:43:14 -0800

    Use latest concurrency tag

--- Package.swift
+++ Package.swift
@@ -20,9 +20,8 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/Alamofire/Alamofire.git",
-                .branch("feature/async-handlers")
-        )
+            url: "https://github.com/Alamofire/Alamofire.git", from: "5.5.0"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
